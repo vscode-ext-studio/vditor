5fba6fce6a7b24a62f888179169d33354b10a2bb
cweijan <weijancc@gmail.com>
2026-06-19 15:19:37 +0800

    Upgrade vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,47 +1,57 @@
-import {resolve} from "path";
-import {defineConfig} from "vite";
-import {viteStaticCopy} from "vite-plugin-static-copy";
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 import pkg from "./package.json";
 
-const isMethodBuild = process.env.VDITOR_BUILD === "method";
+export default defineConfig(({ mode }) => {
+  const dest = mode === "development" ? "dist" : ".";
+  const isMethodBuild = process.env.VDITOR_BUILD === "method";
 
-export default defineConfig({
-  define: {
-    VDITOR_VERSION: JSON.stringify(pkg.version),
-  },
-  build: {
-    outDir: "dist",
-    emptyOutDir: !isMethodBuild,
-    minify: "terser",
-    target: "es2015",
-    lib: {
-      entry: resolve(__dirname, isMethodBuild ? "src/method.ts" : "src/index.ts"),
-      formats: ["umd"],
-      name: "Vditor",
-      fileName: () => (isMethodBuild ? "method.min.js" : "index.min.js"),
+  return {
+    define: {
+      VDITOR_VERSION: JSON.stringify(pkg.version),
     },
-    rollupOptions: {
-      output: {
-        exports: "default",
-        assetFileNames: "index.css",
+    css: {
+      preprocessorOptions: {
+        less: {
+          javascriptEnabled: true,
+        },
       },
     },
-  },
-  css: {
-    preprocessorOptions: {
-      less: {
-        javascriptEnabled: true,
+    server: {
+      port: 3135,
+      host: "127.0.0.1"
+    },
+    build: {
+      outDir: "dist",
+      emptyOutDir: !isMethodBuild,
+      target: "es2015",
+      lib: {
+        entry: resolve(__dirname, isMethodBuild ? "src/method.ts" : "src/index.ts"),
+        formats: ["umd"],
+        name: "Vditor",
+        fileName: () => (isMethodBuild ? "method.min.js" : "index.min.js"),
+      },
+      rolldownOptions: {
+        output: {
+          exports: "default",
+          assetFileNames: "index.css",
+        },
       },
     },
-  },
-  plugins: isMethodBuild ? [] : [
-    viteStaticCopy({
-      targets: [
-        {src: "src/css", dest: "."},
-        {src: "src/images", dest: "."},
-        {src: "src/js", dest: "."},
-        {src: "types", dest: "."},
-      ],
-    }),
-  ],
+    plugins: [
+      ...(mode === "production" && isMethodBuild
+        ? []
+        : [
+          viteStaticCopy({
+            targets: [
+              { src: "src/css", dest },
+              { src: "src/images", dest },
+              { src: "src/js", dest },
+              { src: "types", dest },
+            ],
+          }),
+        ]),
+    ],
+  };
 });
